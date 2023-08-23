8f8c7e6369ff23120546b247d1ed8ba71b446add
Jinkyu Yoon <cmefly97@gmail.com>
2023-08-23 12:20:31 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,57 @@
+#pip install streamlit openai
+'''
+    streamlit은 파이썬으로 편리하게 웹 UI를 제작할 수 있도록 도와주는 도구 - html/javascript를 배우지 않아도 된다.
+    Openai 는 openai LLM 도구
+
+    참고 :  코드 자동완성안되는 이슈 - streamlit reportmissingimports
+     - pip install 'python-language-server[all]'
+'''
+
+import streamlit as st
+import openai 
+
+openai.api_key = st.secrets["api_key"]
+
+st.title("ChatGPT Plus DALL-E")
+# streamlit 테스트 : 터미널에서  streamlit run app.py 실행
+
+with st.form("form") :
+    user_input = st.text_input("Prompt")
+    size = st.selectbox("Size",["1024x1024","512x512","256x256"])
+    submit = st.form_submit_button("Submit")
+
+if submit and user_input:
+    #st.write(user_input)
+    gpt_prompt = [{
+        "role": "system",
+        "content": "Imagine the detail appearance of the input. Response it shortly around 20 words."
+    }]
+    gpt_prompt.append({ 
+        "role": "user",
+        "content": user_input
+    })
+
+    with st.spinner("Waiting for ChatGPT ..."):
+        gpt_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=gpt_prompt
+        )
+
+    prompt = gpt_response["choices"][0]["message"]["content"]
+
+    st.write(prompt)
+
+
+    with st.spinner("Waiting for Dall-E ..."):
+        dally_response = openai.Image.create(
+            prompt = prompt,
+            size=size
+        )
+
+    img = dally_response["data"][0]["url"]
+
+    st.write(img)
+    st.image(img)
+
+
+
